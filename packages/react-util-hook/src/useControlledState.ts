fbc36a4f4b63e05a37e1daa8d521bdf98c992ce4
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2023-03-02 16:33:58 +0800

    release: v0.0.24

--- packages/react-util-hook/src/useControlledState.ts
+++ packages/react-util-hook/src/useControlledState.ts
@@ -36,7 +36,7 @@ export function useControlledState<T, R = T>(option: {
 
   useLayoutUpdateEffect(() => {
     if (isUndefined(value)) {
-      setInnerValue(value as unknown as T)
+      setInnerValue(value!)
     }
   }, [value])
 
