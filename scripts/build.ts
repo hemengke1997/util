dd367b21e86bb343b2d08c1d77e1ce4935793511
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2023-03-06 10:48:25 +0800

    fix: build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,7 @@
 import type { Options } from 'tsup'
 import { build as tsupBuild } from 'tsup'
 import { getTsconfig } from 'get-tsconfig'
-import { isObject } from '@minko-fe/lodash-pro'
+import { isObject, isUndefined } from '@minko-fe/lodash-pro'
 import { defaultConfig } from '../tsup.config'
 
 async function build(opts: Options = {}) {
@@ -9,7 +9,8 @@ async function build(opts: Options = {}) {
 
   const tsconfig = getTsconfig()
 
-  if (isObject(dts)) {
+  if (isObject(dts) || isUndefined(dts)) {
+    dts = {}
     dts = {
       ...dts,
       compilerOptions: {
