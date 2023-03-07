c7f4d16990696fdc0d9565c0362222d3da05f0a1
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2023-03-07 15:31:05 +0800

    feat(react-component): default box-shadow

--- packages/react-component/tailwind.config.cjs
+++ packages/react-component/tailwind.config.cjs
@@ -47,6 +47,9 @@ module.exports = {
         motionEaseInQuint: 'cubic-bezier(0.755, 0.05, 0.855, 0.06)',
         motionEaseOutQuint: 'cubic-bezier(0.23, 1, 0.32, 1)',
       },
+      boxShadow: {
+        default: '0 6px 16px 0 rgb(0 0 0 / 8%), 0 3px 6px -4px rgb(0 0 0 / 12%), 0 9px 28px 8px rgb(0 0 0 / 5%)',
+      },
     },
   },
 
