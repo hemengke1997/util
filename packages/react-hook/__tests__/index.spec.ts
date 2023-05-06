1513044937877d9a70bd4a1ea5d1ec85724c429f
myx <15984587583@163.com>
2023-05-06 23:40:45 +0800

    feat: useControlledState spec

--- packages/react-hook/__tests__/index.spec.ts
+++ packages/react-hook/__tests__/index.spec.ts
@@ -1,6 +1,9 @@
+// @vitest-environment jsdom
+
 import { describe, expect, test } from 'vitest'
 import { act, renderHook } from '@testing-library/react'
 import { useStrictInput } from '../src/useStrictInput'
+import { useControlledState } from '../src/useControlledState'
 
 describe('condition', () => {
   test('test', () => {
@@ -8,16 +11,80 @@ describe('condition', () => {
   })
 })
 
+describe('useControlledState', () => {
+  it('should be return default value', () => {
+    const { result } = renderHook(() =>
+      useControlledState({
+        defaultValue: 'defaultValue',
+      })
+    )
+
+    const [value] = result.current
+
+    expect(value).toBe('defaultValue')
+  })
+
+  it('should be return value', () => {
+    const { result } = renderHook(() =>
+      useControlledState({
+        value: 'value',
+        defaultValue: 'defaultValue',
+      })
+    )
+
+    const [value] = result.current
+
+    expect(value).toBe('value')
+  })
+
+  it('should be callback onchange that is controlled state', () => {
+    let val = ''
+    const { result } = renderHook(() =>
+      useControlledState({
+        value: 'value',
+        defaultValue: 'defaultValue',
+        onChange: (value, preValue) => {
+          val = value
+        },
+      })
+    )
+
+    const [value, setValue] = result.current
+
+    setValue('controlled')
+
+    expect(val).toBe('controlled')
+  })
+
+  it('should not be callback onchange that is not controlled state', () => {
+    let val = ''
+    const { result } = renderHook(() =>
+      useControlledState({
+        defaultValue: 'defaultValue',
+        onChange: (value, preValue) => {
+          val = value
+        },
+      })
+    )
+
+    const [value, setValue] = result.current
+
+    setValue('controlled')
+
+    expect(val).toBe('')
+  })
+})
+
 describe('useStrictInput', () => {
   it('should only keep number', () => {
-    let value = '123'
+    let value = ''
     const { result } = renderHook(() =>
       useStrictInput(value, (newValue) => {
         value = newValue
       })
     )
     act(() => {
-      result.current.onChange('aslkdj123')
+      result.current.onChange('asd123~!@#$ ][];.,/?"*-+')
     })
 
     expect(value).toBe('123')
