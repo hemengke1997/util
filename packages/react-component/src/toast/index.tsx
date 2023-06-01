f5f422a4ab856484a126e53ad85c258c27cb8cc4
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2023-06-01 11:08:59 +0800

    chore: release v0.0.71-beta.6

--- packages/react-component/src/toast/index.tsx
+++ packages/react-component/src/toast/index.tsx
@@ -3,6 +3,6 @@ import { toast } from './method'
 
 export default toast
 export { toast }
-export type { ToastType, ToastPosition, ToastOptions } from './PropsType'
+export type { ToastType, ToastOptions } from './PropsType'
 export { ToastContext } from './ToastContext'
 export type { ToastContextProps } from './ToastContext'
